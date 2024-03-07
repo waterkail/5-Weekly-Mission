def6b257f45f4ab9d1c061c9c965e0a0f28e1c6c
이현승 <superkail13@gmail.com>
2024-03-07 10:46:36 +0900

    로그인 관련 자바스크립트 코드 제작 완료

--- signup.js
+++ signup.js
@@ -3,9 +3,23 @@ const passwordInput = document.querySelector('#password');
 const passCheckInput = document.querySelector('#passwordcheck');
 const passEyeButton = document.querySelector('.passEye');
 const checkEyeButton = document.querySelector('.passCheckEye');
+const signButton = document.querySelector('.signbutton');
 
-let A = 0;
-let B = 0;
+let A = 0; /* 비밀번호 보이기/숨기기 함수용 변수, 좋은 변수명 구함11 */
+let B = 0; /* 비밀번호 확인 보이기/숨기기 함수용 변수, 좋은 변수명 구함22 */
+
+function loginEmailErrorReset() {
+  document.querySelector('.giveMeEmail').classList.add('hidden');
+  document.querySelector('.notEmail').classList.add('hidden');
+  document.querySelector('.checkEmail').classList.add('hidden');
+  document.querySelector('#email').classList.remove('inputProblem');
+} /* 로그인 - 이메일 오류 메시지를 초기화 하는 함수  */
+
+function loginPassErrorReset() {
+  document.querySelector('.checkPassword').classList.add('hidden');
+  document.querySelector('.giveMePassword').classList.add('hidden');
+  document.querySelector('#password').classList.remove('inputProblem');
+} /* 로그인 - 비밀번호 오류 메시지를 초기화 하는 함수 */
 
 function isEmail(email) {
   const A = email.indexOf('@');
@@ -20,9 +34,7 @@ function isEmail(email) {
 } /* 이메일이 맞으면 true를 뱉는 함수*/
 
 const emailPls = function (e) {
-  document.querySelector('.giveMeEmail').classList.add('hidden');
-  document.querySelector('.notEmail').classList.add('hidden');
-  document.querySelector('#email').classList.remove('inputProblem');
+  loginEmailErrorReset();
   if (emailInput.value === '') {
     document.querySelector('.giveMeEmail').classList.remove('hidden');
     document.querySelector('#email').classList.add('inputProblem');
@@ -33,8 +45,7 @@ const emailPls = function (e) {
 }; /* 이메일을 입력하지 않거나 이메일 주소로 입력하지 않을 때 반응하는 함수 */
 
 const passwordPls = function (e) {
-  document.querySelector('.giveMePassword').classList.add('hidden');
-  document.querySelector('#password').classList.remove('inputProblem');
+  loginPassErrorReset();
   if (passwordInput.value === '') {
     document.querySelector('.giveMePassword').classList.remove('hidden');
     document.querySelector('#password').classList.add('inputProblem');
@@ -43,6 +54,7 @@ const passwordPls = function (e) {
 
 emailInput.addEventListener('focusout', emailPls);
 passwordInput.addEventListener('focusout', passwordPls);
+
 passEyeButton.onclick = function () {
   passEyeButton.firstElementChild.classList.toggle('hidden');
   passEyeButton.lastElementChild.classList.toggle('hidden');
@@ -51,6 +63,23 @@ passEyeButton.onclick = function () {
     passwordInput.type = 'text';
   } else {
     passwordInput.type = 'password';
+  }
+}; /* 비밀번호 칸에서 눈을 눌러 비밀번호 보이기와 숨기기를 하게 하는 함수이자 이벤트 */
+
+signButton.onclick = function (e) {
+  e.preventDefault();
+  loginEmailErrorReset();
+  loginPassErrorReset();
+  if (
+    emailInput.value === 'test@codeit.com' &&
+    passwordInput.value === 'codeit101'
+  ) {
+    location.href = './folder';
+  } else {
+    document.querySelector('.checkPassword').classList.remove('hidden');
+    document.querySelector('#password').classList.add('inputProblem');
+    document.querySelector('.checkEmail').classList.remove('hidden');
+    document.querySelector('#email').classList.add('inputProblem');
   }
 };
 
@@ -63,4 +92,4 @@ checkEyeButton.onclick = function () {
   } else {
     passCheckInput.type = 'password';
   }
-};
+}; /* 비밀번호 확인 칸에서 눈을 눌러 비밀번호 보이기와 숨기기를 하게 하는 함수이자 이벤트 */
