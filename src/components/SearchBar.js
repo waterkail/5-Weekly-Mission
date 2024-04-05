afe3559d932dcaaae4e38875a61bb3db5f19dee6
이현승 <superkail13@gmail.com>
2024-04-05 12:23:09 +0900

    searchBar 컴포넌트 수정

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ import searchIcon from '../asset/Search.svg';
 const BAR_COLOR = '#f5f5f5';
 const BAR_BORDER_RADIUS = '10px';
 
-const BarOfSearch = styled.div`
+const BarOfSearch = styled.form`
   display: flex;
   padding: 15px 16px;
   border-radius: ${BAR_BORDER_RADIUS};
@@ -28,10 +28,42 @@ const SearchInput = styled.input`
   outline: none;
 `;
 
+const HiddenButton = styled.button`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip-path: inset(50%);
+  border: 0;
+  clip: rect(0 0 0 0);
+`;
+
+const HiddenLabel = styled.label`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip-path: inset(50%);
+  border: 0;
+  clip: rect(0 0 0 0);
+`;
+
 const SearchBar = ({ palceholder }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchingLink = document.querySelector(`#search`);
+    console.log(searchingLink.value);
+  };
+
   return (
-    <BarOfSearch>
-      <SearchInput placeholder={palceholder} />
+    <BarOfSearch onSubmit={handleSubmit}>
+      <HiddenLabel htmlFor="search">검색</HiddenLabel>
+      <SearchInput id="search" placeholder={palceholder} />
+      <HiddenButton type="submit">검색하기</HiddenButton>
     </BarOfSearch>
   );
 };
